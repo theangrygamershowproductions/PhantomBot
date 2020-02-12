b61cc238a949328406894fed3810578bb15318c8
reesey275 <reesey275@gmail.com>
2020-02-12 15:27:36 -0500

    Updated Twitter API (TAGS ALLOWED)

--- source/com/illusionaryone/TwitterAPI.java
+++ source/com/illusionaryone/TwitterAPI.java
@@ -170,7 +170,7 @@ public class TwitterAPI {
         }
 
         try {
-            Status status = twitter.updateStatus(statusString.replaceAll("@", "").replaceAll("#", ""));
+            Status status = twitter.updateStatus(statusString);
             com.gmt2001.Console.debug.println("Success");
             return "true";
         } catch (TwitterException ex) {
@@ -192,7 +192,7 @@ public class TwitterAPI {
         }
 
         try {
-            StatusUpdate statusUpdate = new StatusUpdate(statusString.replaceAll("@", "").replaceAll("#", ""));
+            StatusUpdate statusUpdate = new StatusUpdate(statusString);
             statusUpdate.setMedia(new File(filename));
             Status status = twitter.updateStatus(statusUpdate);
             com.gmt2001.Console.debug.println("Success");
